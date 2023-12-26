4db42aa347679508a379491ca9d281471ac28e09
codewithJerry <singhsame798@gmail.com>
2023-12-26 22:59:28 +0530

    Music_player

---
+++ script.js
@@ -0,0 +1,67 @@
+const btn = document.querySelector(".btn_play");
+const nextbtn = document.querySelector("[value='next']");
+const prevbtn = document.querySelector("[value='prev']");
+const progress = document.querySelector(".progress");
+const image = document.querySelector("img");
+var audio = document.querySelector('audio'); // Update with the correct filename of your audio file
+let rotation_count = 0, Id; // to count and control 
+
+function stopRotation() {
+  image.style.transform = 'rotate(' + rotation_count + 'deg)';
+}
+
+function startRotation() {
+  image.style.transform = 'rotate(' + rotation_count + 'deg)';
+}
+
+btn.addEventListener("click", () => {
+  if (btn.value === "play") {
+    audio.play();
+    Id = setInterval(() => {
+      startRotation();
+      rotation_count += 1;
+    }, 100);
+    btn.value = "pause";
+  } else {
+    audio.pause();
+    stopRotation();
+    clearInterval(Id);
+    btn.value = "play";
+  }
+});
+
+audio.addEventListener("timeupdate", () => {
+  progress.value = audio.currentTime;
+});
+
+progress.addEventListener("change", () => {
+  audio.currentTime = progress.value;
+});
+
+nextbtn.addEventListener("click", () => {
+  
+  if(audio.currentTime+10<audio.duration){
+
+    
+    console.log("before",audio.currentTime);
+
+    
+    audio.currentTime=audio.currentTime+10;
+
+    
+    console.log("after",audio.currentTime);
+    
+  }
+  else{
+    console.log("end of audio");
+    audio.pause();
+  }
+
+});
+
+prevbtn.addEventListener("click", () => {
+
+  audio.currentTime -= 10;
+  console.log("After: audio.currentTime =", audio.currentTime);
+});
+
